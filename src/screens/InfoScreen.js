a9a347627f65a152ee5c5d063f9056ab81c98b69
LovelaceFilson <58467784+LovelaceFilson@users.noreply.github.com>
2020-12-13 18:01:43 -0800

    Created Blog List Screen

---
+++ src/screens/InfoScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+
+function ListScreen({ navigation }) {
+  return <View style={styles.container}></View>;
+}
+
+export default ListScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
---
+++ src/screens/ListScreen.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { StyleSheet, View, FlatList } from "react-native";
+import BlogItem from "../component/BlogItem";
+
+function ListScreen({ navigation }) {
+  const blogposts = [
+    {
+      blogImage: require("../assets/akshar-dave-1GRvY9WUu08-unsplash.jpg"),
+      blogTitle: "Does Dry in January Actually Improve Your Health?",
+      authorImage: require("../assets/profile.jpg"),
+      authorName: "Kylie Jenner",
+      readTime: "4",
+    },
+    {
+      blogImage: require("../assets/amy-shamblen-qdPnQuGeuwU-unsplash.jpg"),
+      blogTitle: "How to Seem Like You Always Have Your Shot Together",
+      authorImage: require("../assets/profile.jpg"),
+      authorName: "Ada Lacey",
+      readTime: "8",
+    },
+    {
+      blogImage: require("../assets/dose-juice-ocnsb17U6FE-unsplash.jpg"),
+      blogTitle: "Does Dry is January Actally Improve Your Health?",
+      authorImage: require("../assets/profile.jpg"),
+      authorName: "Will Smith",
+      readTime: "90",
+    },
+    {
+      blogImage: require("../assets/amy-shamblen-lJt-3NUFng4-unsplash.jpg"),
+      blogTitle: "You do hire a designer to make something. You Hire Them.",
+      authorImage: require("../assets/profile.jpg"),
+      authorName: "Kendall Jenner",
+      readTime: "13",
+    },
+  ];
+  return (
+    <View style={styles.container}>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={blogposts}
+        renderItem={({ item }) => {
+          return <BlogItem post={item} />;
+        }}
+        keyExtractor={(item, index)=>index}
+      />
+    </View>
+  );
+}
+
+export default ListScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 10,
+  },
+  info: {
+    fontSize: 16,
+  },
+  groupicons: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    backgroundColor: "white",
+  },
+});
